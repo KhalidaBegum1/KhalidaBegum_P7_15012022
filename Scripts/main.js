33fd17151be577878a0e1964a266d920365f6c02
Khalida Begum <k.begum1@gmail.com>
2022-01-24 15:50:06 +0100

    dynamique cards

--- Scripts/main.js
+++ Scripts/main.js
@@ -4,6 +4,15 @@ const mainSearch = document.querySelector("#main-search_input");
 const secondarySearch = document.querySelectorAll(".label-input");
 const selectedTagsContainer = document.getElementById("selected-tags-list");
 
-secondarySearch.forEach((input) =>
+/*secondarySearch.forEach((input) =>
   input.addEventListener("click", openDropList)
-);
+);*/
+
+const recipeSection = document.querySelector("#recipes");
+recipeSection.innerHTML = "";
+
+for (let i = 0; i < recipes.length; i++) {
+  recipeSection.innerHTML +=
+    '<div class="recipe-card">' + recipes[i].name + "</div>";
+}
+
