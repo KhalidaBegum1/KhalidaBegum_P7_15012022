05e2ecbe45547e0be888d4956aa647b603e8e8f3
Khalida Begum <k.begum1@gmail.com>
2022-01-24 16:13:20 +0100

    algo simple

--- Scripts/main.js
+++ Scripts/main.js
@@ -3,16 +3,30 @@ import { recipes } from "./recipes.js";
 const mainSearch = document.querySelector("#main-search_input");
 const secondarySearch = document.querySelectorAll(".label-input");
 const selectedTagsContainer = document.getElementById("selected-tags-list");
+const searchBtn = document.querySelector("#main-search_btn");
+const recipeSection = document.querySelector("#recipes");
+recipeSection.innerHTML = "";
+
+searchBtn.addEventListener("click", search);
+mainSearch.addEventListener("change", search);
+function search(event) {
+  const word = mainSearch.value;
+  if (word.length >= 3) {
+    recipeSection.innerHTML = "";
+    for (let i = 0; i < recipes.length; i++) {
+      if (recipes[i].name.toLowerCase().includes(word.toLowerCase())) {
+        recipeSection.innerHTML +=
+          '<div class="recipe-card">' + recipes[i].name + "</div>";
+      }
+    }
+  }
+}
 
 /*secondarySearch.forEach((input) =>
   input.addEventListener("click", openDropList)
 );*/
 
-const recipeSection = document.querySelector("#recipes");
-recipeSection.innerHTML = "";
-
 for (let i = 0; i < recipes.length; i++) {
   recipeSection.innerHTML +=
     '<div class="recipe-card">' + recipes[i].name + "</div>";
 }
-
