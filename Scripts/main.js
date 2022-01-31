fd1081fc762a49f91db77da5a26b7d8cc08cc04e
Khalida Begum <k.begum1@gmail.com>
2022-01-31 15:26:56 +0100

    filters

--- Scripts/main.js
+++ Scripts/main.js
@@ -14,14 +14,57 @@ function search(event) {
     recipeSection.innerHTML = "";
     for (let i = 0; i < recipes.length; i++) {
       if (recipes[i].name.toLowerCase().includes(word.toLowerCase())) {
-        recipeSection.innerHTML +=
-          '<div class="recipe-card">' + recipes[i].name + "</div>";
+        let textIngredients = "";
+        for (let j = 0; j < recipes[i].ingredients.length; j++) {
+          let ingredient = recipes[i].ingredients[j];
+          textIngredients += ` ${ingredient.ingredient}
+          <span> ${ingredient.quantity}</span> 
+          <span>${ingredient.unit}</span> `;
+        }
+        recipeSection.innerHTML += `<div class="recipe-section" >
+        <span class="title-section" ><div class="recipe-name">${recipes[i].name}  </div>
+        <span class="recipe-time"><i class="far fa-clock"></i> ${recipes[i].time} min </span> </span>
+        <span class="text-section"> <div class="recipe-ingredient"> 
+       ${textIngredients}</div>
+         <div class="recipe-description">  ${recipes[i].description}  </div></span>
+        </div>`;
+      } else if (
+        recipes[i].description.toLowerCase().includes(word.toLowerCase())
+      ) {
+        let textIngredients = "";
+        for (let j = 0; j < recipes[i].ingredients.length; j++) {
+          let ingredient = recipes[i].ingredients[j];
+          textIngredients += ` ${ingredient.ingredient}
+          <span> ${ingredient.quantity}</span> 
+          <span>${ingredient.unit}</span> `;
+        }
+        recipeSection.innerHTML += `<div class="recipe-section" >
+        <span class="title-section" ><div class="recipe-name">${recipes[i].name}  </div>
+        <span class="recipe-time"><i class="far fa-clock"></i> ${recipes[i].time} min </span> </span>
+        <span class="text-section"><div class="recipe-ingredient">
+       ${textIngredients}</div>
+         <div class="recipe-description">  ${recipes[i].description}  </div></span>
+        </div>`;
       }
-      
     }
   }
 }
+
 /*
+ {
+        recipeSection.innerHTML +=
+          '<div class="recipe-description">' +
+          recipes[i].description +
+          "</div>";
+      }
+
+ if (recipes.description.toLowerCase().indexOf(word.toLowerCase())) {
+        recipeSection.innerHTML +=
+          '<div class="recipe-description">' +
+          recipes[i].description +
+          "</div>";
+      }
+    }
 let ingredients = document.querySelectorAll(recipes.ingredients);
 for (let i = 0; i < ingredients.length; i++){
   if (recipes[i].ingredients.toLowerCase().includes(word.toLowerCase())) {
@@ -29,8 +72,6 @@ for (let i = 0; i < ingredients.length; i++){
     '<div class="recipe-card">' + recipes[i].ingredients + "</div>";
   }
 }*/
-
-
 
 //let ingredients = document.querySelectorAll(recipes.ingredients);
 //let ingredients = ingredients.toString();
