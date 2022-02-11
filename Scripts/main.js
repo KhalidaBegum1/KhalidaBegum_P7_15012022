627137850961db0f1cbf1866241bef05ba63d196
Khalida Begum <k.begum1@gmail.com>
2022-02-11 18:08:17 +0100

    algo simple complete

--- Scripts/main.js
+++ Scripts/main.js
@@ -38,36 +38,53 @@ function search(event) {
           <span> ${ingredient.quantity}</span> 
           <span>${ingredient.unit}</span> `;
         }
-        recipeSection.innerHTML = `<div class="recipe-section" >
+        recipeSection.innerHTML += `<div class="recipe-section" >
         <span class="title-section" ><div class="recipe-name">${recipes[i].name}  </div>
         <span class="recipe-time"><i class="far fa-clock"></i> ${recipes[i].time} min </span> </span>
         <span class="text-section"><div class="recipe-ingredient">
        ${textIngredients}</div>
          <div class="recipe-description">  ${recipes[i].description}  </div></span>
         </div>`;
+      } else {
+        let ingredientFiltered = [];
+        for (let k = 0; k < recipes[i].ingredients.length; k++) {
+          if (
+            recipes[i].ingredients[k].ingredient
+              .toLowerCase()
+              .includes(word.toLowerCase())
+          ) {
+            ingredientFiltered.push(recipes[i].ingredients[k]);
+          }
+        }
+
+        if (ingredientFiltered.length > 0) {
+          let textIngredients = "";
+          for (let j = 0; j < recipes[i].ingredients.length; j++) {
+            let ingredient = recipes[i].ingredients[j];
+            textIngredients += ` ${ingredient.ingredient}
+          <span> ${ingredient.quantity}</span> 
+          <span>${ingredient.unit}</span> `;
+          }
+          recipeSection.innerHTML += `<div class="recipe-section" >
+        <span class="title-section" ><div class="recipe-name">${recipes[i].name}  </div>
+        <span class="recipe-time"><i class="far fa-clock"></i> ${recipes[i].time} min </span> </span>
+        <span class="text-section"><div class="recipe-ingredient">
+       ${textIngredients}</div>
+         <div class="recipe-description">  ${recipes[i].description}  </div></span>
+        </div>`;
+        }
       }
     }
   }
 }
 
-let i = 0;
-//recipes.length;
-//i++;
-
-let j = 0;
-//recipes[i].ingredients.length;
-//j++;
-
-/*let i = recipes.length ++;
-let j = recipes.ingredients.length ++;
-let ingredient = recipes[i].ingredients[j];*/
-let ingredient = recipes[i].ingredients[j];
-let textIngredients = "";
-textIngredients += ` ${ingredient.ingredient}
-<span> ${ingredient.quantity}</span> 
-<span>${ingredient.unit}</span> `;
-
 recipes.forEach((recipes) => {
+  let textIngredients = "";
+  recipes.ingredients.forEach((ingredient) => {
+    textIngredients += ` ${ingredient.ingredient}
+    <span> ${ingredient.quantity}</span> 
+    <span>${ingredient.unit}</span> `;
+  });
   recipeSection.innerHTML += ` 
   <div class="recipe-section" >
   <span class="title-section" ><div class="recipe-name">${recipes.name}  </div>
@@ -79,17 +96,24 @@ recipes.forEach((recipes) => {
   `;
 });
 
-const dropSearch = document.querySelectorAll(".label-btn");
+/*let ingredientFiltered = recipes[i].ingredients.filter((ing) =>
+          ing.ingredient.toLowerCase().includes(word.toLowerCase())
+        );*/
+
+let dropSearch = document.querySelectorAll(".label-btn");
 const closeSearch = document.querySelector(".search-close");
 const dropIngredient = document.querySelector(".search-ingredient");
 const dropAppareil = document.querySelector(".search-appareil");
 const dropUtensil = document.querySelector(".search-utensil");
-dropSearch.forEach((btn) =>
+const igBtn = document.getElementById("ingredients-btn");
+const appBtn = document.getElementById("appareils-btn");
+const utenBtn = document.getElementById("utensils-btn");
+
+dropSearch.forEach((btn) => {
   btn.addEventListener("click", () => {
     dropIngredient.style.display = "block";
-    console.log("test");
-  })
-);
+  });
+});
 
 function closeBox() {
   dropIngredient.style.display = "none";
